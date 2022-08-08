541d3ad667b9160d2a30481dd11b1ee309c51d03
scottwwh <github@scottwwh.ca>
2022-08-07 21:37:29 -0400

    Cleaned up export script

--- mongodb/export.sh
+++ mongodb/export.sh
@@ -1,14 +1,9 @@
+
 mongoexport -u root -p example --authenticationDatabase admin --jsonFormat=canonical \
     --db=test --collection=cats --out=./opt/cats.json
-# mongoimport -u root -p example --authenticationDatabase admin \
-#     --db=test_indexed --collection=indexed_cats --file=cats.json
 
 mongoexport -u root -p example --authenticationDatabase admin --jsonFormat=canonical \
     --db=test --collection=breeders --out=./opt/breeders.json
-# mongoimport -u root -p example --authenticationDatabase admin \
-#     --db=test_indexed --collection=indexed_breeders --file=breeders.json
 
 mongoexport -u root -p example --authenticationDatabase admin --jsonFormat=canonical \
     --db=test --collection=owners --out=./opt/owners.json
-# mongoimport -u root -p example --authenticationDatabase admin \
-#     --db=test_indexed --collection=indexed_owners --file=owners.json
