3f17646e2d200ae31d756d5552e75931b818bfea
scottwwh <github@scottwwh.ca>
2022-08-10 07:52:55 -0400

    Cleanup

--- web/src/query.js
+++ web/src/query.js
@@ -4,15 +4,10 @@ const MONGO_DATABASE = 'test';
 const MONGO_HOST_A = 'mongodb://localhost:27017'; // Default
 const MONGO_HOST_B = 'mongodb://localhost:27018'; // Indexed
 
-
 const SET_DEFAULT = 'default';
 const SET_INDEXED = 'indexed';
-
-
 const SETS = [
     {
-        // const results_default = [];
-        // const results_indexed = [];
         id: SET_DEFAULT,
         host: MONGO_HOST_A
     },
@@ -20,16 +15,6 @@ const SETS = [
         id: SET_INDEXED,
         host: MONGO_HOST_B
     }
-
-    // MONGO_HOST_A,
-    // MONGO_HOST_B
-];
-
-
-// Remap the above
-const SET_IDS = [
-    SET_DEFAULT,
-    SET_INDEXED
 ];
 
 
@@ -52,8 +37,6 @@ async function runTest(Model, filter) {
     }
 }
 
-// const results_default = [];
-// const results_indexed = [];
 
 
 /**
@@ -62,10 +45,13 @@ async function runTest(Model, filter) {
  * At the moment, I need to manually update these values which is crazy.. so gotta fix this!
  * 
  * Ideally, I'd randomly select ~10 breeders and owners, and do something fancy?
+ * 
+ * What's super interestins is that when both of these are used,
+ * we only get a few results yet the indexed database is ~95% faster?
  */
 
-// What's super interestins is that when both of these are used,
-// we only get a few results yet the indexed database is ~95% faster?
+import CatModel from './models/CatModel.js';
+
 const breederId = "62f2e2a741732eb268e4d5bc";
 const ownerId = "62f2e2a841732eb268e4d766";
 const catName = "Incubate";
@@ -78,9 +64,10 @@ const filter = {
 };
 
 
-///
+// const cats = await CatModel.find({});
+// console.log('Cats?', cats);
 
-import CatModel from './models/CatModel.js';
+
 for (var i = 0; i < SETS.length; i++) {
     try {
         const host = SETS[i].host;
@@ -96,11 +83,8 @@ for (var i = 0; i < SETS.length; i++) {
             const data = await runTest(CatModel, filter);
 
             // Create array if missing
-            // results_default.push(data);
             if (!results[id]) {
-                // results[name].push(data);
                 results[id] = [];
-            // } else {
             }
 
             results[id].push(data);
@@ -112,45 +96,14 @@ for (var i = 0; i < SETS.length; i++) {
       }
 }
 
-// Debug
-// console.log('RESULTS!', results);
-
-
-//////
-
-
-/*
-try {
-    const db2 = await mongoose.connect(MONGO_HOST_B, {
-        user: "root",
-        pass: "example",
-        dbName: MONGO_DATABASE
-    });
-} catch (err) {
-    console.log(err);
-}
-
-
-// This may be a good way to mimic where we need caching (or where Mongo provides it?)
-for (var i = 0; i < RUNS; i++) {
-    const data = await runTest(CatModel, filter);
-    results_indexed.push(data);
-}
-
-
-mongoose.disconnect();
-*/
-
 
 // REPORTING
+// console.log('RESULTS!', results);
 
-
+// This should be higher in the file
 const RUNS = 50;
 
-
 function summarize(arr) {
-    // console.log(arr);
-
     let total = 0;
     arr.forEach(el => {
         total += el.ms;
@@ -164,17 +117,17 @@ function summarize(arr) {
 }
 
 
-// const summaryDefault = summarize(results['default']);
-// const summaryIndexed = summarize(results['indexed']);
 const summaryDefault = summarize(results['default']);
 const summaryIndexed = summarize(results['indexed']);
 
+const improvement = Math.round((1 - (summaryIndexed.average / summaryDefault.average)) * 100);
+const msg = `
+# Results
 
-const msg = `Results:
-
-    Results: ${summaryDefault.results}
-    Average (default): ${summaryDefault.average} ms 
-    Average (indexed): ${summaryIndexed.average} ms
-    % of default: ${summaryIndexed.average / summaryDefault.average}
+Reporting on ${summaryDefault.results}
+Average (default): ${summaryDefault.average} ms 
+Average (indexed): ${summaryIndexed.average} ms
+Improvement vs default: ${improvement}%
 `;
 console.log(msg);
+
